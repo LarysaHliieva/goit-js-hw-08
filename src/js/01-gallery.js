81cddc5d964fa160b79629b250e9ac2b97e26112
LarysaHliieva <lora.glievaya@gmail.com>
2022-11-30 20:55:52 +0200

    update task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+lightbox.on('show.simplelightbox', function (e) {
+    e.preventDefault(); 
+});
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+        `
+        <a
+          class="gallery__item"
+          href="${original}"
+          rel="noopener noreferrer nofollow"
+
+        >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            title="${description}"
+          />
+        </a>
+    `).join('');
+}
